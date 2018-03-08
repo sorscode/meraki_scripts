1ba6bcbacf08def6da5df468db04b581fa61c8d3
sorscode <sorscode@gmail.com>
2018-03-08 16:17:04 -0600

    Minor check fixes

--- client_usage_info.py
+++ client_usage_info.py
@@ -12,11 +12,11 @@ from vars import apikey, organizationid
 
 # Global Variables
 devicemodel = 'MX' # MR/MS/MX/MV
-max = 10   # Set this for the maximum number of results you want to see
-time_24hr = 86400 # 86400 = 24hr
-time_days = 30 # How many days do you want data usage, 30 is max
+max = 10           # Adjustable so you can test on a small subset before running against entire organization
+time_24hr = 86400  # 86400 = 24hr
+time_days = 30     # How many days do you want data usage
 total_time = time_24hr * time_days
-vlan_check = 1 # Specify the VLAN you want Data Usage from
+vlan_check = 1     # Specify the VLAN you want Data Usage from
 
 # Global Dictionaries
 filtered = []
@@ -26,14 +26,16 @@ clientinfo = []
 
 def get_org_inv():
     print('Get Device List')
-    # define local variable(s) for the function
+    # define local variables for the function
     orglist = m.getorginventory(apikey, organizationid, suppressprint=True)
     count = 0
     # Time to get a list of all the networks
     for row in orglist:
         model = row['model']
-        if model[:2] == devicemodel: # Filter out only the network types we want to see
-            if count < max:
+        if model[:2] == devicemodel and count < max: # Filter out only the network types we want to see
+            if row['networkId'] is None:
+                print('Device is not associated with a Network')
+            else:
                 filterresults.append({'networkId': row['networkId'],'serial': row['serial'], 'model': row['model']})
                 count += 1
                 print(str(count) + " - " + row['serial'] + " - " + row['networkId'])
@@ -41,35 +43,36 @@ def get_org_inv():
 def get_network_info():
     print('Getting Network Information')
     for row in filterresults:
-        # define local variable(s) for the function
         fac_number = m.getnetworkdetail(apikey, row['networkId'], suppressprint=True)
-        # getnetworkdetail, will give you information related to a specific network 
         networkinfo.append({'name': fac_number['name'], 'serial': row['serial'] })
         print(fac_number['name'] + " - " + row['serial'])
 
 def get_client_info():
     print('Get Client list')
     for row in networkinfo:
-        # define local variable(s) for the function
+        # print(row['name']) # Print statement to make sure there was data
         data_usage = m.getclients(apikey, row['serial'], timestamp=total_time, suppressprint=True)
-        # getclients, will give you data usage for users 
-        for i in data_usage:
-            clientinfo.append(
-                {'name': row['name'], \
-                'description': i['description'], \
-                'mac': i['mac'], \
-                'vlan': i['vlan'], \
-                'sent': i['usage']['sent'], \
-                'recv': i['usage']['recv']})
+        if data_usage is None:
+            print('Nothing to see here')
+        else:
+            for i in data_usage:
+                # print(data_usage) # Print statement to make sure there was data
+                clientinfo.append(
+                    {'name': row['name'], \
+                    'description': i['description'], \
+                    'mac': i['mac'], \
+                    'vlan': i['vlan'], \
+                    'sent': i['usage']['sent'], \
+                    'recv': i['usage']['recv']})
 
-def print_client_info(): # Quick view of Data received, helps with debugging
+def print_client_info():
     for row in clientinfo:
         if row['vlan'] is 150:
             print(row['name'] + " - " + str(row['description']) + " - " + row['mac'] + " - " + str(row['vlan']))
 
-def filter_data(): # Going to filter out the data I want into its own list/dict
+def filter_data():
     for row in clientinfo:
-        if row['vlan'] is vlan_check:
+        if row['vlan'] == vlan_check:
             filtered.append(
                 {'name': row['name'], \
                 'description': row['description'], \
@@ -79,11 +82,11 @@ def filter_data(): # Going to filter out the data I want into its own list/dict
                 'recv': row['recv']}
             )
 
-def output_client_csv(): # Output all the data to a CSV
+def output_client_csv():
     timestr = time.strftime("%Y-%m-%d-%H%M%S")
     with open('client_list-' + timestr + '.csv', 'a') as outcsv:
         writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        writer.writerow(['Location','Description', 'MAC Address', 'Vlan', 'Sent - Kb', 'Received - Kb'])
+        writer.writerow(['Facility','Description', 'MAC Address', 'Vlan', 'Sent - Kb', 'Received - Kb'])
         for item in filtered:
             if item['description'] is None:
                 writer.writerow([item['name'], 'No Description', item['mac'], item['vlan'], item['sent'], item['recv']])
@@ -101,3 +104,4 @@ def main():
     output_client_csv()
 
 main()
+
